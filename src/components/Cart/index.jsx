e660b270fb8edea3ebc2cc8519472a4625979053
Ibragim.Chekmazov <ronsver123@gmail.com>
2023-12-20 19:00:16 +0100

    add to cart little UI

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,18 +1,50 @@
 import React from "react";
 import CartItem from "../CartItem";
 import { useSelector } from "react-redux";
+import styles from "./index.module.css";
 
 function Cart() {
   const cart = useSelector((state) => state.cart.list);
-  const totalSum = cart.reduce((acc, el) => acc + el.price * el.count, 0);
+  const totalSum = cart
+    .reduce((acc, el) => acc + el.price * el.count, 0)
+    .toFixed(2);
 
   return (
-    <div>
-      {cart.map((el) => (
-        <CartItem key={el.id} {...el} />
-      ))}
+    <div className={styles.mainCart}>
+      <div className={styles.productsCart}>
+        {cart.map((el) => (
+          <CartItem key={el.id} {...el} />
+        ))}
+      </div>
 
-      <h3>{totalSum}</h3>
+      <div className={styles.orderMenu}>
+        <h3 className={styles.orderH3}>Order details</h3>
+        <div className={styles.infoOrder}>
+          <p className={styles.pCart}>
+            3 items <br /> Total
+          </p>
+          <h2 className={styles.orderH2}>{`$${totalSum ? totalSum : ""}`}</h2>
+        </div>
+
+        <div className={styles.inputsOrdersBox}>
+          <input
+            className={styles.inputsOrders}
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            className={styles.inputsOrders}
+            type="text"
+            placeholder="Phone number"
+          />
+          <input
+            className={styles.inputsOrders}
+            type="text"
+            placeholder="Email"
+          />
+        </div>
+        <button className={styles.btnOrder}>Order</button>
+      </div>
     </div>
   );
 }
--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -1,21 +1,51 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { countMinus, countPlus, funDelete } from "../../store/slices/cartSlice";
+import styles from "./index.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
-function CartItem({ title, id, price, count, discont_price }) {
+function CartItem({ title, id, price, count, discont_price, image }) {
   const dispatch = useDispatch();
-
-  const allSum = price * count;
 
   return (
     <div>
-      <div>
-        <p>{title}</p>
-        <p>{count}</p>
-        <p>{price}</p>
-        <button onClick={() => dispatch(countMinus(id))}>-</button>
-        <button onClick={() => dispatch(countPlus(id))}>+</button>
-        <span onClick={() => dispatch(funDelete(id))}>X</span>
+      <div className={styles.ordersCards}>
+        <img
+          className={styles.imgOrder}
+          src={`http://localhost:3333${image}`}
+          alt=""
+        />
+        <div className={styles.infoCards}>
+          <p className={styles.pTitle}>{title}</p>
+
+          <div className={styles.aboutProductDiv}>
+            <div className={styles.countBox}>
+              <button
+                className={styles.btnCount}
+                onClick={() => dispatch(countMinus(id))}
+              >
+                <FontAwesomeIcon icon={faMinus} />
+              </button>
+              <p className={styles.pCount}>{count}</p>
+              <button
+                className={styles.btnCount}
+                onClick={() => dispatch(countPlus(id))}
+              >
+                <FontAwesomeIcon icon={faPlus} />
+              </button>
+            </div>
+
+            <p className={styles.pDiscontPrice}>{`$${discont_price}`}</p>
+            <p className={styles.pPrice}>{`$${price}`}</p>
+            <span
+              className={styles.btnDelete}
+              onClick={() => dispatch(funDelete(id))}
+            >
+              x
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
